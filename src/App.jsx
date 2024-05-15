b974a2b43eeca4e9c70da809ef92cfeb356818d2
Giulio-Marra <giuliomarramail@gmail.com>
2024-05-15 11:28:17 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import PostsHome from "./components/PostsHome";
-import { Container } from "react-bootstrap";
 
-// import MyNavbar from "./components/MyNavbar";
-// import ProfilePage from "./components/ProfilePage";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Homepage from "./components/Homepage";
+import MyNavbar from "./components/MyNavbar";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <>
-      <Container>
-        <PostsHome />
-      </Container>
-      {/* <BrowserRouter>
-        <MyNavbar query="me" />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter> */}
+      <MyNavbar query="me" />
+
+      <ProfilePage />
     </>
   );
 }
